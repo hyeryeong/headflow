245a3eabae839d8eba64941ea89efaa623310f82
hyeryeong <hyeryeong.yoon@headflow.net>
2011-03-02 19:21:57 +0900

    headflow Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Introduce ED's Friends l Home")
     end
   end
 
@@ -14,6 +20,21 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Introduce ED's Friends l Contact")
+    end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Introduce ED's Friends l About")
+    end
 end
