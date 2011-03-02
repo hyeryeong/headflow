7305d8ae43a8d3983248abb6e2823de21918979f
hyeryeong <hyeryeong.yoon@headflow.net>
2011-03-02 13:17:57 +0900

    headflow Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headflow::Application.config.secret_token = 'a05f2e48aba7becc0554557169b44422960189d3a965c91b6180cfd6cc80ba54f705a54e1d807104de36906b1d5442ec033fcea4ae09fc37b4ebb37f86fb989a'
